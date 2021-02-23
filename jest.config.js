68247d989f9b1fdca4876e6716cc9650705a808b
Makar <68788448+tinkermakar@users.noreply.github.com>
2021-02-24 03:48:49 +0400

    same

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'jest-puppeteer',
   setupFilesAfterEnv: ['./jest/server.js'],
   reporters: ['default', 'jest-junit'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'],
 };
